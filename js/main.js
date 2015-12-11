d44162d425a426643d74fbbdb5e09cd3285c2a85
IronStalker <v_reznichenko@ukr.net>
2015-12-11 15:20:32 +0200

    using fancybox

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-$('.dropdown-toggle').dropdown()
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы
+    PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+    $("#popup2").show();
+    $("#popup3").show();
+    $("#popup4").show();
+    $("#popup5").show();
+    $("#popup6").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+    $("#popup2").hide();
+    $("#popup3").hide();
+    $("#popup4").hide();
+    $("#popup5").hide();
+    $("#popup6").hide();
+}
 
-$('#myTab a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
 
-$(".alert").alert()
 
+$(".popup").fancybox({
+});
