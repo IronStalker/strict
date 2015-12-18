d307687d4fdf0f7c338f788fd7ae0fdecb71cc8f
IronStalker <v_reznichenko@ukr.net>
2015-12-18 17:21:01 +0200

    fancybox added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
-$(document).ready(function(){
-    //Скрыть PopUp при загрузке страницы
-    PopUpHide();
-});
-//Функция отображения PopUp
-function PopUpShow(){
-    $("#popup1").show();
-    $("#popup2").show();
-    $("#popup3").show();
-    $("#popup4").show();
-    $("#popup5").show();
-    $("#popup6").show();
-}
-//Функция скрытия PopUp
-function PopUpHide(){
-    $("#popup1").hide();
-    $("#popup2").hide();
-    $("#popup3").hide();
-    $("#popup4").hide();
-    $("#popup5").hide();
-    $("#popup6").hide();
-}
-
-
-
-$(".popup").fancybox({
+$(document).ready(function () {
+    $(".popup").fancybox({
+        helpers: {
+            overlay: {
+                locked: false
+            }
+        }
+    });
 });
